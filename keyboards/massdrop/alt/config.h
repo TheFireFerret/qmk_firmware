9eee35d887fdba092c6d8069754d955e788b7d14
Sami Glasco <samiglasco@gmail.com>
2021-05-30 23:38:30 -0700

    NO_USB_STARTUP_CHECK round 3

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -113,6 +113,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* disable print */
 //#define NO_PRINT
 
+/* disable usb startup check */
+#define NO_USB_STARTUP_CHECK
+
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
